4ac720c5324d147c93f102d6b4e253697ba45986
sandeep-luhaniwal <sandeepluh0001.com>
2025-05-27 10:48:05 +0530

    data show enter

--- server.js
+++ server.js
@@ -51,6 +51,10 @@ app.post("/submit", async (req, res) => {
   }
 });
 
+app.get("/data", (req, res) => {
+  res.json(submissions); // assuming `submissions` is an array storing data
+});
+
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
